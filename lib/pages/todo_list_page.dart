53b13260f409616e138d8fe2de475ba1d3f114da
lucaspl <lucaslopes@unitins.br>
2022-02-23 13:27:34 -0300

    adicionando um campo de input e um botao.

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+  TodoListPage({Key? key}) : super(key: key);
+
+  final TextEditingController tarefaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container( color: Colors.green, );
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: tarefaController,
+                decoration: InputDecoration(
+                  labelText: 'Nova Tarefa',
+                  hintText: 'Adicione uma nova tarefa',
+                  //border: OutlineInputBorder(),
+                  errorText: null,
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: login,
+                  child: Text('Entrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    String text = tarefaController.text;
+    print(text);
   }
 }
